379daa74f09c29b852319fc2afb937c2c71f81a3
salamer <salamer_gaga@163.com>
2018-01-18 15:28:24 +0800

    add db.tree persist

--- persist.go
+++ persist.go
@@ -1,1 +1,26 @@
 package Juicy
+
+import (
+	"fmt"
+
+	rbt "github.com/salamer/RbTree"
+)
+
+func (db *DB) Persist(filename string) {
+	node, err := db.Tree.GetRoot()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		db.serialize(filename, node)
+	}
+}
+
+func (db *DB) serialize(filename string, node *rbt.Node) {
+	fmt.Println(node)
+	if node.Left != nil {
+		db.serialize(filename, node.Left)
+	}
+	if node.Right != nil {
+		db.serialize(filename, node.Right)
+	}
+}
